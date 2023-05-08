5835722b705a765dfb33935cf62d056c0ee9f993
sm970309 <sm970309@gmail.com>
2023-05-08 14:56:06 +0900

    kss split-sentence 적용

--- server/capstone_server/views.py
+++ server/capstone_server/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import sys
+from kss import split_sentences
 
 sys.path.append('../')
 from modules import yt,stt
@@ -35,9 +36,11 @@ def check(request):
         return HttpResponse("error occurred")
     res_id = stt.useAPI(audio_file_path)
     text = stt.makeTextline(res_id)
-    
+    text = text['results']['utterances'][0]['msg']
+    text = split_sentences(text)
+    print(text)
     # 프론트 연동 테스트용
     # result = {'result': url}
     # return JsonResponse(result)
     # html에서 한글 깨지는 거 수정
-    return JsonResponse(text,json_dumps_params={'ensure_ascii': False}, status=200)
+    return JsonResponse({"text":text},json_dumps_params={'ensure_ascii': False}, status=200)
