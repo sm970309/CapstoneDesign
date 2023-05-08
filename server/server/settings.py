e4caaaa308f28338944556002e06efe32a6c41c9
sm970309 <sm970309@gmail.com>
2023-05-08 20:03:55 +0900

    requirements.txt 파일 이동

--- server/server/settings.py
+++ server/server/settings.py
@@ -23,10 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h=w!-=snxbe8!94xnzle8kl==d86o#9e@5+6p6!iw+!6@7=vnk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
 
 # Application definition
 
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
