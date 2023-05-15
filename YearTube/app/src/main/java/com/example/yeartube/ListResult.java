ddf0c0da33bbbff661a35ee62cee6f80149d7226
HyeonLang <kjkw5657@inu.ac.kr>
2023-05-15 19:30:59 +0900

    UI

---
+++ YearTube/app/src/main/java/com/example/yeartube/ListResult.java
@@ -0,0 +1,9 @@
+package com.example.yeartube;
+
+public class ListResult {
+    String result;
+    String title;
+    int num_problem_sentences;
+    
+
+}
---
+++ YearTube/app/src/main/java/com/example/yeartube/Result.java
@@ -0,0 +1,5 @@
+package com.example.yeartube;
+
+public class Result {
+
+}
